e17bdbcae6d3cddba27359f41c777e3e54837808
cjmartin89 <cjmartin89@hotmail.com>
2017-09-10 01:10:16 -0400

    Create Quotes Api

--- quotes/api/urls.py
+++ quotes/api/urls.py
@@ -8,7 +8,7 @@ from .views import (
 app_name = 'quotes'
 
 urlpatterns = [
-    url(r'^$', QuotesListAPIView.as_view(), name='api-list'),  # api/wrong
+    url(r'^$', QuotesListAPIView.as_view(), name='api-list'),  # api/quotes
     url(r'^create/$', QuotesCreateAPIView.as_view(), name='api-create'),  # api/create
     url(r'^(?P<pk>[0-9]+)/$', QuotesDetailView.as_view(), name='api-detail')
 ]
