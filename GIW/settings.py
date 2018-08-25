d5ff976e695429e45a7915fcd67575fd81c9a3ca
Christopher Martin <martintechsolution@gmail.com>
2018-08-25 16:44:16 -0400

    Updated Debug Settings

--- GIW/settings.py
+++ GIW/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(%emz)hc^4t_ql0ubnfso89e+se)$x3!###x2)-&qi4q6njbn%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '71.196.39.30', '73.244.255.225', '*']
 
